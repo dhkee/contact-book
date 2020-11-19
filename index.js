70b215067f1493f9f4e689e673a581c4b6b95268
dhkee <dhkee@mcnc.co.kr>
2020-11-19 17:17:47 +0900

    show, edit, update, destroy added

--- index.js
+++ index.js
@@ -53,9 +53,37 @@ app.get('/contacts', function(req, res){
 app.get('/contacts/new', function(req, res){
     res.render('contacts/new');
 });
-// Conctacts - create
+// Contacts - create
 app.post('/contacts', function(req, res){
     Contact.create(req.body, function(err, contact){// DB에 data를 생성하는 함수
+        if(err) return res.json(err);
+        res.redirect('/contacts');
+    });
+});
+// Contacts - Show
+app.get('/contacts/:id', function(req, res){        // route에 콜론(:)을 사용하면 해당 위치의 값을 받아 req.params에 넣음.
+    Contact.findOne({_id : req.params.id}, function(err, contact){  // DB에서 해당 model의 document를 하나 찾는 함수
+        if(err) return res.json(err);
+        res.render('contacts/show', {contact : contact});
+    });
+});
+// Contacts - Edit
+app.get('/contacts/:id/edit', function(req, res){
+    Contact.findOne({_id : req.params.id}, function(err, contact){
+        if(err) return res.json(err);
+        res.render('contacts/edit', {contact : contact});
+    });
+});
+// Contacts - Update
+app.post('/contacts/:id/update', function(req, res){
+    Contact.findOneAndUpdate({_id : req.params.id}, req.body, function(err, contact){   // DB에서 해당 model의 document를 하나 찾아 그 data를 수정하는 함수
+        if(err) return res.json(err);
+        res.redirect('/contacts/'+req.params.id);
+    });
+});
+// Contacts - Destroy
+app.post('/contacts/:id/destroy', function(req, res){
+    Contact.deleteOne({_id : req.params.id}, function(err){         // DB에서 해당 model의 document를 하나 찾아 삭제하는 함수
         if(err) return res.json(err);
         res.redirect('/contacts');
     });
